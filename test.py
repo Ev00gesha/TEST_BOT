3dade6c095fe66848e27fb22e23d0790971dad19
Ev00gesha <rakzena180@gmail.com>
2022-01-25 14:11:16 +0300

    "UPD"

--- test.py
+++ test.py
@@ -14,12 +14,12 @@ def test_time(message):
         spis_time_day[i] = int(spis_time_day[i])
     spis_time_day[0] += 3
     
-    wb = load_workbook("testchd.xlsx")
+    wb = load_workbook("testshd.xlsx")
     sheet = wb["TEST"]
 
     for i in range(sheet.max_row):
-        spis_time_shd = sheet["C" + str(i + 1)].value.split(":")
-        if int(spis_time_day[0]) <= int(spis_time_shd[0] and int(spis_time_day[1]) < int(spis_time_shd[1])):
+        spis_time_shd = str(sheet["B" + str(i + 1)].value).split(":")
+        if int(spis_time_day[0]) <= int(spis_time_shd[0]) and int(spis_time_day[1]) < int(spis_time_shd[1]):
             bot.send_message(
                 message.chat.id,
                 f"Следующая игра команды TEST\nДата: {sheet['A' + str(i + 1)].value}\nВремя: {sheet['C' + str(i + 1)].value}\nПротив команды {sheet['B' + str(i + 1)].value}"
@@ -27,5 +27,5 @@ def test_time(message):
             break
         else:
             continue
-        
+
 bot.polling(none_stop=True, interval=0)
